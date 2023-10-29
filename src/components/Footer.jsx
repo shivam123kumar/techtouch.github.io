ee9a81e8b5223324ac54e55bb3a2cf6d29730d2a
Shivam kumar <kumarshivam843113@gmail.com>
2023-10-29 13:58:17 +0530

    techtoouch

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechTouch</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtu.be/jpK5hYdTve8?si=Sv1cVjGc6amjxGGE" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.linkedin.com/in/shivam-kumar-26ba1b220/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/shivam123kumar" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
